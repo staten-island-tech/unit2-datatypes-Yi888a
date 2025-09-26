7cc79dbc0d115513908cd59535f7d49d4c567257
Yi888a <Ziyil8@nycstudents.net>
2025-09-26 09:22:45 -0400

    finished

--- test.py
+++ test.py
@@ -20,10 +20,41 @@
    # tip = 0
 #print(f"Your tip is {tip}, and your total is {bill + tip}")
 
-import math
-def gcf(a, b):
-    return math.gcd(a, b)
-user_input = input("input two numbers, separated by a comma. ")
-num1 = int(user_input.split(",")[0])
-num2 = int(user_input.split(",")[1])
-print(gcf(num1, num2))
+#import math
+#def gcf(a, b):
+    #return math.gcd(a, b)
+#user_input = input("input two numbers, separated by a comma. ")
+#num1 = int(user_input.split(",")[0])
+#num2 = int(user_input.split(",")[1])
+#print(gcf(num1, num2))
+
+#def factor():
+    #factors = []
+    #number = int(input("What number do you want to factor? "))
+    #for i in range(1, number + 1):
+       # if number % i == 0:
+       #     factors.append(i)
+ #   print(factors)
+
+#factor()
+
+#def odd_even():
+   # number = int(input("What number do you want to check? "))
+    #if number % 2 == 0:
+        #print("Even")
+  #  else:
+       # print("Odd")
+#odd_even()
+
+def guess_number():
+    import random
+    number = random.randint(1, 10)
+    guess = int(input("Guess a number between 1 and 10. "))
+    while guess != number:
+        if guess < number:
+            print("Too low.")
+        else:
+            print("Too high.")
+        guess = int(input("Guess again. "))
+    print("Correct!")
+guess_number()
